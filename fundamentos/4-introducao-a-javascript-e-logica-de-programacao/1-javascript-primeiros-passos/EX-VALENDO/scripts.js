084d86c67c6a080a6d2223424e3f9ab79ee34a3f
Daniel Bozza <danielbozza4@gmail.com>
2022-03-09 11:43:47 -0300

    adicionando exercicios da aula  do dia 1 bem como o de lanchonete da monitoria

---
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/1-javascript-primeiros-passos/EX-VALENDO/scripts.js
@@ -0,0 +1,102 @@
+// Ex1
+
+// const a = 4;
+// const b = 3;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// const adicao = (a + b);
+
+// let requisitos = "\
+// Adição (a + b)n\n\
+// Subtração (a - b)\n\
+// Multiplicação (a * b)\n\
+// Divisão (a / b)\n\
+// Módulo (a % b)";
+
+// console.log(requisitos);
+// console.log(adicao)
+
+// const idadeGabriel = 50;
+
+// if (idadeGabriel == 30) {return "nao";
+// } else if (idadeGabriel == 50) {return "aposentar";
+// } else {return "sim";
+// }
+
+// switch (idadeGabriel) {
+//     case 30: return "nao";
+//     case 50: return "aposentar";
+//     default: return "sim";
+// }
+
+//Ex2 e 3
+// const a = 4;
+// const b = 2;
+// const c = 3;
+
+// if (a > b){
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+
+// if (a > b && a > c){
+//     console.log(a);
+// } else if (b > a && b > c) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+// Ex aula Mega sena
+// let numerosMegaSena = [1, 8, 6, 34, 17, 60];
+// let meusNumeros = [6, 15, 22, 38, 42, 57];
+
+// for (let i = 0; i < numerosMegaSena.lenght; i += 1) {
+//         console.log('Numero mega-sena: ', numerosMegaSena[i]);
+//     for (let z = 0; z < meusNumeros.lenght; z += 1){
+//         if(numerosMegaSena[i] === meusNumeros[z]) {
+//             console.log("Parabéns!! você acertou o numero ", meusNumeros[z]);
+//         }
+//     }
+// }
+
+// Ex 4
+// const pos = 1;
+// if (pos >= 0){
+//     console.log("positivo");
+// } else{
+//         console.log("negativo");
+//     }
+
+// Ex5 - triangulo
+
+const a1 = 10;
+const a2 = 45;
+const a3 = 45;
+
+if (a1 + a2 + a3 === 180) {
+    console.log("Parabéns, é um triangulo!!");
+} else if (a1 <=0 || a2 <= 0 || a3 <= 0) {
+    console.log("Existe não esse angulo");
+} else {
+    console.log("Deu não, mano vei");
+}
+
+function()
+
+// ex 8
+const n1 = 2
+const n2 = 4
+const n3 = 1
+
+if (n1 % 2 === 1 || n2 % 2 === 1 || n3 % 2 === 1){
+    console.log(true);
+} else{
+    console.log(false);;
+}
