109e7051682b4173c01c8e3372c64a026b5aa2ab
Daniel Bozza <danielbozza4@gmail.com>
2022-03-18 21:18:17 -0300

    adicionando o inferno do item 5git add scripts.js  consegui mas ainda duvida

--- fundamentos/5-Dom-eventos-webStorage-JS/3-eventos/ex-pos-aula/scripts.js
+++ fundamentos/5-Dom-eventos-webStorage-JS/3-eventos/ex-pos-aula/scripts.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-
+//Ex1
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 for(let i = 0; i < dezDaysList.length; i += 1) {
@@ -28,12 +28,15 @@ for(let i = 0; i < dezDaysList.length; i += 1) {
     }
     document.getElementById('days').appendChild(dias);
 }
+
+//EX2
     let button = document.createElement('button');
 function botao(Feriados) {
     button.innerText = Feriados;
     document.getElementsByClassName('buttons-container')[0].appendChild(button);
 }
 
+//EX3
 button.addEventListener('click', corFeriados)
 let feriados = document.querySelectorAll('.holiday');
 function corFeriados() {
@@ -49,6 +52,51 @@ function corFeriados() {
     }
 }
 
+//EX4
+let BTNSexta = document.createElement('button');
+function criarBTNSexta(string) {
+    BTNSexta.id = "btn-friday";
+    BTNSexta.innerText = string;
+    document.getElementsByClassName('buttons-container')[0].appendChild(BTNSexta);
+}
+
+//EX5
+BTNSexta.addEventListener('click', sextou);
+let sextas = document.getElementsByClassName('friday');
+let sextasArray = [];
+for(let i = 0; i < sextas.length; i += 1) {
+    let item = sextas[i].innerText;
+    sextasArray.push(item);
+}
+function sextou() {
+    for(let i = 0; i < sextas.length; i += 1) {
+        if (sextas[i].innerText.includes('SEXTOU')) {
+            sextas[i].innerHTML = sextasArray[i];
+        }
+        else {
+            sextas[i].innerText = 'SEXTOU !';
+        }
+    }
+}
+
+// EX5 NAO ENTENDI PQ NAO DEU
+// BTNSexta.addEventListener('click', sextou);
+// let sextas = document.getElementsByClassName('friday');
+// let newtxt = 'SEXTOU !'
+// function sextou() {
+//     for(let i = 0; i < sextas.length; i += 1) {
+//         if (sextas[i].innerText.includes('SEXTOU')) {
+//             sextas[i].innerHTML = document.getElementsByClassName('friday')[i];
+//             sextas[i].classList.remove('alter');
+//         }
+//         else {
+//             sextas[i].innerText = newtxt;
+//             sextas[i].className += ' alter';
+//         }
+//     }
+// }
 
 
 botao('Feriados')
+criarBTNSexta("Sexta-feira")
+
